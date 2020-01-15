8e1d74fb19582be1508c29b80bb1b3a3138de4d5
Denis <dennetesin@gmail.com>
2020-01-15 22:45:35 +0200

    finish rect group move finish lines group select/unselect

--- src/app/services/undo-redo-canvas.service.ts
+++ src/app/services/undo-redo-canvas.service.ts
@@ -1,158 +1,128 @@
-import {Injectable} from '@angular/core';
-import {CanvasService} from './canvas.service';
-import {UndoRedoService} from './undo-redo.service';
-import {Layer} from 'konva/types/Layer';
-import {ActionType} from '../luwfy-canvas/undo-redo.interface';
-import {IGroupCustom, IPathCustom} from '../luwfy-canvas/shapes-interface';
-import {Collection} from 'konva/types/Util';
-import {Group} from 'konva/types/Group';
-import {Shape, ShapeConfig} from 'konva/types/Shape';
-import {theme} from '../luwfy-canvas/theme';
+import { Injectable } from '@angular/core';
+import { CanvasService } from './canvas.service';
+import { UndoRedoService } from './undo-redo.service';
+import { Layer } from 'konva/types/Layer';
+import { ActionType } from '../luwfy-canvas/undo-redo.interface';
+import { IGroupCustom, IPathCustom } from '../luwfy-canvas/shapes-interface';
+import { Collection } from 'konva/types/Util';
+import { Group } from 'konva/types/Group';
+import { Shape, ShapeConfig } from 'konva/types/Shape';
+import { theme } from '../luwfy-canvas/theme';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable ( {
+    providedIn: 'root',
+} )
 export class UndoRedoCanvasService {
-
-  constructor(private canvasService: CanvasService, private undoRedoService: UndoRedoService) {
-
-
-  }
-
-
-  performUndo(mainLayer: Layer, activeGroup: Group, activePathArr?: IPathCustom[]) {
-
-    if (this.undoRedoService.undoRedoArr && this.undoRedoService.undoRedoArr.length > 0) {
-      let last_action_obj = this.undoRedoService.undoRedoArr[this.undoRedoService.undoRedoArr.length - 1];
-      switch (last_action_obj.action) {
-        case ActionType.Move : {
-          // if(last_action_obj.object.hasChildren() && last_action_obj.object.length> 0 )
-          // {
-          //
-          //
-          //   (last_action_obj.object as IGroupCustom).x(last_action_obj.coordinates.x);
-          //   (last_action_obj.object as IGroupCustom).y(last_action_obj.coordinates.y);
-          //
-          // }
-
-          console.log('[c] X ', last_action_obj.coordinates.x);
-          console.log('[c] Y ', last_action_obj.coordinates.y);
-
-          console.log('[c] cccc', last_action_obj.object);
-
-          (last_action_obj.object as IGroupCustom).x(last_action_obj.coordinates.x);
-          (last_action_obj.object as IGroupCustom).y(last_action_obj.coordinates.y);
-
-
-          (last_action_obj.object as IGroupCustom).fire('dragmove', () => {
-          });
-          console.log('action move');
-          this.undoRedoService.deleteLastAction();
-          break;
-        }
-        case ActionType.Delete : {
-          if (!last_action_obj.parent) {
-            let temp_parent: IGroupCustom;
-            (last_action_obj.object as IPathCustom[]).forEach((elem) => {
-              temp_parent = this.canvasService.getGroupById(elem.attrs.start_info.start_group_id, mainLayer.getStage());
-              temp_parent.add(elem);
-              this.canvasService.addElemToActivePathArr(elem);
-
-            });
-
-
-          } else {
-            (last_action_obj.object as Collection<Group | Shape<ShapeConfig>>).each((elem) => {
-              last_action_obj.parent.add(elem);
-            });
-
-
-          }
-
-          this.undoRedoService.deleteLastAction();
-          break;
-        }
-        case ActionType.Create : {
-          if ((last_action_obj.object as IPathCustom).className === 'Path') {
-            (last_action_obj.object as IPathCustom).remove();
-
-
-          } else {
-
-            (last_action_obj.object as IGroupCustom).remove();
-
-
-          }
-
-
-          this.undoRedoService.deleteLastAction();
-
-          break;
-        }
-        case ActionType.Select : {
-          if ((last_action_obj.object as IPathCustom).className === 'Path') {
-            (last_action_obj.object as IPathCustom).setAttr('stroke', theme.line_color);
-
-            this.canvasService.removeLastElementFromPathArr();
-          }
-          this.undoRedoService.deleteLastAction();
-          break;
-        }
-
-        case ActionType.Unselect : {
-
-          if ((last_action_obj.parent as Group)._id === activeGroup._id) {
-            if ((last_action_obj.object as IGroupCustom[]).length && (last_action_obj.object as IGroupCustom[]).length > 0) {
-              console.log('unselect 3');
-              (last_action_obj.object as IGroupCustom[]).forEach((elem) => {
-                console.log('unselect 4');
-
-
-                elem.parent.setAttr('x', elem.position().x - activeGroup.position().x);
-                elem.parent.setAttr('y', elem.position().y - activeGroup.position().y);
-                elem.add(last_action_obj.parent);
-
-                elem.setAttr('draggable', false);
-                // activeGroup.setAttr('visible', true);
-                elem.children.each((elem) => {
-                  if (elem.className && elem.className === 'Path') {
-                    elem.setAttr('stroke', theme.line_color);
-                  } else {
-                    elem.setAttr('stroke', theme.choose_group_color);
-                  }
-
-                });
-
-
-                last_action_obj.parent.setAttr('draggable', true);
-
-              });
-            } else if ((last_action_obj.object as IPathCustom[]).length && (last_action_obj.object as IPathCustom[]).length > 0 && last_action_obj.object[0].className) {
-
-
-            }
-
-
-          } else {
-
-            (last_action_obj.object as IGroupCustom).add(last_action_obj.parent);
-
-          }
-
-          this.undoRedoService.deleteLastAction();
-          break;
-
-
-        }
-
-
-      }
-
-
+    
+    constructor ( private canvasService: CanvasService, private undoRedoService: UndoRedoService ) {
+    
     }
-
-
-  }
-
-
+    
+    performUndo ( mainLayer: Layer, activeGroup: Group, activePathArr?: IPathCustom[] ) {
+        
+        if ( this.undoRedoService.undoRedoArr && this.undoRedoService.undoRedoArr.length > 0 ) {
+            let last_action_obj = this.undoRedoService.undoRedoArr[ this.undoRedoService.undoRedoArr.length - 1 ];
+            switch ( last_action_obj.action ) {
+                case ActionType.Move : {
+                    (last_action_obj.object as IGroupCustom).x ( last_action_obj.coordinates.x );
+                    (last_action_obj.object as IGroupCustom).y ( last_action_obj.coordinates.y );
+                    
+                    (last_action_obj.object as IGroupCustom).fire ( 'dragmove', () => {
+                    } );
+                    console.log ( 'action move' );
+                    this.undoRedoService.deleteLastAction ();
+                    break;
+                }
+                case ActionType.Delete : {
+                    if ( !last_action_obj.parent ) {
+                        let temp_parent: IGroupCustom;
+                        (last_action_obj.object as IPathCustom[]).forEach ( ( elem ) => {
+                            temp_parent = this.canvasService.getGroupById ( elem.attrs.start_info.start_group_id, mainLayer.getStage () );
+                            temp_parent.add ( elem );
+                            this.canvasService.addElemToActivePathArr ( elem );
+                            
+                        } );
+                        
+                    } else {
+                        (last_action_obj.object as Collection<Group | Shape<ShapeConfig>>).each ( ( elem ) => {
+                            last_action_obj.parent.add ( elem );
+                        } );
+                        
+                    }
+                    
+                    this.undoRedoService.deleteLastAction ();
+                    break;
+                }
+                case ActionType.Create : {
+                    if ( (last_action_obj.object as IPathCustom).className === 'Path' ) {
+                        (last_action_obj.object as IPathCustom).remove ();
+                        
+                    } else {
+                        
+                        (last_action_obj.object as IGroupCustom).remove ();
+                        
+                    }
+                    
+                    this.undoRedoService.deleteLastAction ();
+                    
+                    break;
+                }
+                case ActionType.Select : {
+                    if ( (last_action_obj.object as IPathCustom).className === 'Path' ) {
+                        (last_action_obj.object as IPathCustom).setAttr ( 'stroke', theme.line_color );
+                        
+                        this.canvasService.removeLastElementFromPathArr ();
+                    }
+                    this.undoRedoService.deleteLastAction ();
+                    break;
+                }
+                
+                case ActionType.Unselect : {
+                    
+                    if ( (last_action_obj.parent as Group)._id === activeGroup._id ) {
+                        if ( (last_action_obj.object as unknown as IGroupCustom[]).length && (last_action_obj.object as unknown as IGroupCustom[]).length > 0 ) {
+                            console.log ( 'unselect 3' );
+                            (last_action_obj.object as unknown as IGroupCustom[]).forEach ( ( elem ) => {
+                                elem.setAttr ( 'x', elem.position ().x - activeGroup.position ().x );
+                                elem.setAttr ( 'y', elem.position ().y - activeGroup.position ().y );
+                                
+                                //elem.add ( last_action_obj.parent );
+                                
+                                activeGroup.add(elem);
+                                
+                                elem.setAttr ( 'draggable', false );
+                                // activeGroup.setAttr('visible', true);
+                                elem.children.each ( ( elem ) => {
+                                    if ( elem.className && elem.className === 'Path' ) {
+                                        elem.setAttr ( 'stroke', theme.line_color );
+                                    } else {
+                                        elem.setAttr ( 'stroke', theme.choose_group_color );
+                                    }
+                                    
+                                } );
+                                
+                                last_action_obj.parent.setAttr ( 'draggable', true );
+                                
+                            } );
+                        } else if ( (last_action_obj.object as IPathCustom[]).length && (last_action_obj.object as IPathCustom[]).length > 0 && last_action_obj.object[ 0 ].className ) {
+                        
+                        }
+                        
+                    } else {
+                        
+                        (last_action_obj.object as IGroupCustom).add ( last_action_obj.parent );
+                        
+                    }
+                    
+                    this.undoRedoService.deleteLastAction ();
+                    break;
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 }
